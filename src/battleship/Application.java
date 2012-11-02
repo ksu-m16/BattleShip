7295a85c2d4b4fd7b50c44d4d2d49696e7e615d0
root <root@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-11-02 15:49:29 +0000

    git-svn-id: svn://127.0.0.1/var/cvs/xu/BattleShip/trunk@141 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

--- src/battleship/Application.java
+++ src/battleship/Application.java
@@ -5,7 +5,7 @@ import kship.KSetupStrategy;
 import model.IFieldModel;
 
 public class Application {
-	
+
 	private GameController gc;
 	private SetupController sc;
 	private ISetupStrategy ss1;
@@ -17,62 +17,90 @@ public class Application {
 	
 	
 	private int ngames = 100;
+
 	private int firstPlayerWins = 0;
 	private int winner;
-	
+
 	public void startChampionship() {
+		
+		long tm = System.currentTimeMillis();
+		
 		for (int i = 0; i < ngames; i++) {
 			init();
 			play();
 		}
 		System.out.println(ngames + " games played.");
 		System.out.println("Player 1 wins " + firstPlayerWins + " times.");
-		System.out.println("Player 2 wins " + (ngames - firstPlayerWins) + " times.");
-		
+		System.out.println("Player 2 wins " + (ngames - firstPlayerWins)
+				+ " times.");
+
 		if (firstPlayerWins == (ngames - firstPlayerWins)) {
 			System.out.println("Friendship wins!");
 		}
-		
+
 		else {
 			winner = (firstPlayerWins > ngames / 2) ? 0 : 1;
 			System.out.println("Player " + (winner + 1) + " wins championship");
 		}
+		
+		System.out.println(System.currentTimeMillis() - tm + " ms");
+		
 	}
-	
+
 	public void init() {
 		sc = new SetupController();
 		gc = new GameController();
 
 		// setup fields
-		ss1 = new SetupStrategy();
-		ss2 = new KSetupStrategy();
+//		ss1 = new SetupStrategy();
+//		ss2 = new KSetupStrategy();
+		
+		ss2 = new SetupStrategy();
+		ss1 = new KSetupStrategy();
+		
 		sc.setSetupStrategy(ss1);
 		f1 = sc.setup();
 		sc.setSetupStrategy(ss2);
 		f2 = sc.setup();
 
 		// setup gamecontroller
+
 		str1 = new KGameStategy(1., 2., 3.);
-		str2 = new XuStrategy3();
+		str2 = new XuStrategy4();
+
 		gc.setPlayer1Strategy(str1);
 		gc.setPlayer2Strategy(str2);
 		gc.setPlayer1Model(f1);
 		gc.setPlayer2Model(f2);
 	}
-	
+
 	public void play() {
-		while (gc.next()) {
+		
+		if (f1 == null) {
+			System.out.println("Bad setupStrategy, player 1. Player 2 wins.");
 		}
 		
+		if (f2 == null) {
+			System.out.println("Bad setupStrategy, player 2. Player 1 wins.");
+		}
+		
+		if (f1 == null || f2 == null) {
+			return;
+		}
+		
+		
+		while (gc.next()) {
+		}
+
 		if (gc.getWinner() == 0) {
 			firstPlayerWins++;
 		}
-		
-//		System.out.println();
-//		System.out.println("player 1 field");
-//		f1.printField();
-//		System.out.println();
-//		System.out.println("player 2 field");
-//		f2.printField();
+
+		// System.out.println();
+		// System.out.println("player 1 field");
+		// f1.printField();
+		// System.out.println();
+		// System.out.println("player 2 field");
+		// f2.printField();
 	}
 }
--- src/battleship/SetupController.java
+++ src/battleship/SetupController.java
@@ -1,14 +1,27 @@
 package battleship;
 
+import java.util.List;
+
 import model.FieldModel;
 import model.IFieldModel;
+import model.IPoint;
+import model.IShipDescription;
+import model.State;
 
 public class SetupController {
 
 	private ISetupStrategy ss;
 
 	public IFieldModel setup() {
-		return FieldModel.getFieldModel(ss.getShips());
+		
+		List<IShipDescription> ships = ss.getShips();
+		
+		if (! checkSetup(ships)) {
+			System.out.println("YOU SHELL NOT PASS, CHEATER!!!");
+			return null; 
+		}
+		
+		return FieldModel.getFieldModel(ships);
 	}
 
 	public void setSetupStrategy(ISetupStrategy ss) {
@@ -17,6 +30,72 @@ public class SetupController {
 
 	public ISetupStrategy getSetupStrategy() {
 		return ss;
+	}
+	
+	private boolean checkSetup(List<IShipDescription> ships) {
+		
+		State[][] field = new State[GameDescription.XMAX][GameDescription.YMAX];
+		for (int i = 0; i < GameDescription.XMAX; i++) {
+			for (int j = 0; j < GameDescription.YMAX; j++) {
+				field[i][j] = State.EMPTY;
+			}
+		}	
+		
+		
+		for (IShipDescription s: ships) {
+			if (!SetupHelper.checkShipPlacement(s, field)) {
+				return false;
+			}
+			placeShip(s, field);
+			makePerimeter(s, field);
+			
+		}
+
+		return true;
+	}
+	
+	private void placeShip(IShipDescription s, State[][] field) {
+		List<IPoint> position = s.getPosition();
+		for (IPoint p : position) {
+			field[p.getX()][p.getY()] = State.SHIP;
+		}
+	}
+
+	private void makePerimeter(IShipDescription s, State[][] field) {
+		List<IPoint> position = s.getPosition();
+		for (IPoint p : position) {
+			makePerimeter(p, field);
+		}
+	}
+
+	private void makePerimeter(IPoint p, State[][] field) {
+		int x = p.getX();
+		int y = p.getY();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (getAt(x - 1 + i, y - 1 + j, field) == State.EMPTY) {
+					setAt(x - 1 + i, y - 1 + j, State.FORBIDDEN, field);
+				}
+			}
+		}
+		
+	}
+	
+	private State getAt(int x, int y, State[][] field) {
+		if (x < 0 || y < 0 || x >= GameDescription.XMAX
+				|| y >= GameDescription.YMAX) {
+			return State.EMPTY;
+		}
+		return field[x][y];
+	}
+
+	private void setAt(int x, int y, State st, State[][] field) {
+		if (x < 0 || y < 0 || x >= GameDescription.XMAX
+				|| y >= GameDescription.YMAX) {
+			return;
+		}
+		field[x][y] = st;
+
 	}
 
 }
